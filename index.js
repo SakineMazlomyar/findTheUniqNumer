0b2e0733a9aa14ea849cff33806728c7cbe87728
sakine mazlomyar <mazlomyar12@gmail.com>
2020-08-22 11:15:05 +0200

    updated solution

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 
-
 function findUniq(arr){
-    let uniq = arr.filter((value,i,self)=>{ return self.indexOf(value) === i});
-
   
     let count = {};
     arr.forEach((value)=> {
       count[value]=(count[value]+1) ||1
       
     })
- 
- 
-    for(let i = 0; i<uniq.length; i++){
-        
-       for(let[k,v] of Object.entries(count)){
-         
-         let n = Number(k);
-         if(uniq[i] === n && v == 1){
-          
-           return uniq[i]
-         
-         }
-       }
- 
-    }
 
+    for(let[k,v] of Object.entries(count)){
+      
+      let n = Number(k);
+      if( v == 1){
+      
+        return n
+      
+      }
+    }
+ 
 }
 
 console.log(findUniq([ 0, 1, 1, 1, 1, 1, 1, 1 ]))
